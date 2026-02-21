14841d017d73b45c82a4a5e55a72006f3cbcd8cf
Abdulrahman D Alghamdi <A.D.Alghamdi@outlook.com>
2026-02-21 18:15:47 +0300

    Implement task_wait_n to manage multiple delays
    Defines an async function to spawn tasks and return delays.

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Module that defines a function task_wait_n.
+"""
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawns task_wait_random n times with the specified max_delay.
+    Returns the list of all delays in ascending order.
+    """
+    delays = []
+    # هنا قمنا بتغيير المنادي ليصبح task_wait_random
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    return delays
