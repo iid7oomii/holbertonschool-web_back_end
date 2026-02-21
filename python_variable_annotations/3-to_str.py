b0888e99b82811cdc3b4dbb7a794a7fc28dd91f4
Abdulrahman D Alghamdi <A.D.Alghamdi@outlook.com>
2026-02-21 18:04:20 +0300

    Add type-annotated to_str function for floats

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that defines a type-annotated to_str function.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a given float.
+    """
+    return str(n)
