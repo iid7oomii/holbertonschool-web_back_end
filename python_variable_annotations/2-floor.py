f840a952728b9700dadc8387c22177217570c9c3
Abdulrahman D Alghamdi <A.D.Alghamdi@outlook.com>
2026-02-21 18:02:23 +0300

    Add floor function with type annotations
    Module provides a type-annotated function to calculate the floor of a float.

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module that provides a type-annotated function to calculate the floor of a float.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a given float as an integer.
+    """
+    return math.floor(n)
