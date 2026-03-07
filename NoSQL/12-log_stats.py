2a72f1bd8aae72904e1b74013902eb60c7cef106
Abdulrahman D Alghamdi <A.D.Alghamdi@outlook.com>
2026-03-07 20:38:57 +0300

    Define main function in log_stats.py
    Refactor script to define main function for better structure.

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-""" 12-log_stats.py """
+""" 12-log_stats.py
+Display some stats about Nginx logs stored in MongoDB
+"""
 
 from pymongo import MongoClient
 
-if __name__ == "__main__":
+def main():
+    """Connect to MongoDB and display Nginx log stats."""
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     nginx = db.nginx
@@ -22,3 +25,6 @@ if __name__ == "__main__":
     # Count of GET /status
     status_count = nginx.count_documents({"method": "GET", "path": "/status"})
     print("{} status check".format(status_count))
+
+if __name__ == "__main__":
+    main()
