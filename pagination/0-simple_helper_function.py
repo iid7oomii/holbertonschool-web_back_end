d3796ee50ab328757f39ace30922d9d4e32a6c49
Abdulrahman D Alghamdi <A.D.Alghamdi@outlook.com>
2026-03-07 06:58:55 +0300

    Refactor index_range function for pagination

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,4 +1,9 @@
-def index_range(page, page_size):
+#!/usr/bin/env python3
+"""Pagination helper"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Return start and end indexes for pagination"""
     start = (page - 1) * page_size
-    end = start + page_size
+    end = page * page_size
     return (start, end)
