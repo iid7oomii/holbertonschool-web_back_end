7f18831ed85bc82b3c7b15d5a77a159e15f80440
Abdulrahman D Alghamdi <A.D.Alghamdi@outlook.com>
2026-03-07 07:02:36 +0300

    Update type hint for index_range function

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
+"""Simple pagination module"""
+
 import csv
-from typing import List
+from typing import List, Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return a tuple containing start and end index for pagination"""
     start = (page - 1) * page_size
     end = page * page_size
     return (start, end)
 
 
 class Server:
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initialize server"""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -24,6 +30,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return a page of the dataset"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
