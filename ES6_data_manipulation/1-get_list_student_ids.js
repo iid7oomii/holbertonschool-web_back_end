59e630eea3be01f400817dddaee91da65ff19423
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-04-01 11:47:04 +0300

    Task-1 fix map fuction becuse I can't deal with function as array

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,14 +1,5 @@
-// array return list of objects
-
-export default function getListStudents() {
-  return [{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }];
+// return Ids of the students
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) return [];
+  return students.map((student) => student.id);
 }
-
-
-function getListStudentIds(getListStudents) {
-  return getListStudents.map((student) => student.id);   
-}
-
-console.log(getListStudentIds(getListStudents()));
