1f36700010d79317c2f0a60584337e5f4864d135
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-04-01 13:09:54 +0300

    TAsk-2 fix assignment problem

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
-// Create a function getStudentsByLocation that returns an array of objects who are located in a specific city.
+// Create a function that returns an array of objects who are located in a specific city.
 export default function getStudentsByLocation(students, location) {
-    return location = students.filter((student) => student.location == 'San Francisco');
+  return students.filter((student) => student.location === location);
 }
