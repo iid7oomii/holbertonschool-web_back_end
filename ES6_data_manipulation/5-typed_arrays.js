4a50c7391af6db77de137b04cb3d59ad96c00bb4
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-04-03 15:23:11 +0300

    Task-5 Create a function named  that returns a new ArrayBuffer with an Int8 value at a specific position.

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+// Create a function named createInt8TypedArray that returns a new ArrayBuffer with an Int8 value at a specific position.
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+
+  return view;
+}
