f54b508f6d14f5b19358acc7a74106fe0f30c9b9
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-04-03 15:20:34 +0300

    Task-4 Create a function  that returns an array of students for a specific city with their new grade

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+// Create a function updateStudentGradeByCity that returns an array of students for a specific city with their new grade
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((g) => g.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : "N/A",
+      };
+    });
+}
