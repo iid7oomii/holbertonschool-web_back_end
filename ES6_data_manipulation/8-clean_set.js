bcff79786e523070dd611443823c1e273cb6fbb4
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-04-03 15:31:14 +0300

    Task-8 fix return problem

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 // Create a function that returns a string of all the set values that start with a specific string.
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
+  if (typeof startString !== 'string' || startString === '') return '';
 
   return [...set]
     .filter(value => typeof value === 'string' && value.startsWith(startString))
