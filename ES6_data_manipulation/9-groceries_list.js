7172f40cd18287f35b68fbd24c88d66e8e11ae12
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-04-03 15:33:36 +0300

    Task-9 Create a function that returns a map of groceries with the following items (name, quantity)

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+// Create a function that returns a map of groceries with the following items (name, quantity)
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
