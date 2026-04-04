414351764b9105d0adfbe0b2ea691787fe575e5f
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-04-04 16:44:26 +0300

    Task-8 Write a function that will accept two arguments

---
+++ ES6_promise/8-try.js
@@ -0,0 +1,5 @@
+// Write a function that will accept two arguments
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) throw new Error('cannot divide by 0');
+  return numerator / denominator;
+}
