ca567307b5ca58c647de75b297b9573bab160fb9
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-04-11 21:03:54 +0300

    Task-0: the problem was not replace process.stdout.write so I write it back and only remove the new line

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
 // Function that prints a string to STDOUT
 function displayMessage(print_STDOUT) {
-  console.log(print_STDOUT);
+  process.stdout.write(print_STDOUT);
 }
 module.exports = displayMessage;
