77cb9054076670b07d6ffd5b924c7df517c9c0b4
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-04-11 20:55:33 +0300

    Task-0 fix: two changes one is write console.log not process.stdout.write and the other is remove \n

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
+// Function that prints a string to STDOUT
 function displayMessage(print_STDOUT) {
-  process.stdout.write(print_STDOUT);
-    process.stdout.write("\n");
+  console.log(print_STDOUT);
 }
-module.exports = displayMessage ;
+module.exports = displayMessage;
