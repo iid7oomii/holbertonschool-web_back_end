97f24f481ba98997fb6500b9b20db7a997a370d8
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-04-11 21:50:47 +0300

    Task-1: use process.stdout.write and data event for stdin reading

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,7 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk.toString().trim()}\n`);
-  }
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}`);
 });
 
 process.stdin.on('end', () => {
