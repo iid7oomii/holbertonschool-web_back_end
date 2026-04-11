bce6b729a62ec06acb8eb0a0490f35764b507039
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-04-11 21:06:31 +0300

    Task-0: write back console.log and remove the comment

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,5 +1,4 @@
-// Function that prints a string to STDOUT
 function displayMessage(print_STDOUT) {
-  process.stdout.write(print_STDOUT);
+  console.log(print_STDOUT);
 }
 module.exports = displayMessage;
